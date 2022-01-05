2b2662e101f1f53d0175d95dba31301b3513c7e7
Jimmy Hannan <jim9112@gmail.com>
2022-01-05 13:24:56 -0500

    add nav bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,1 +1,19 @@
+<script>
+	import logo from '../../static/logo.svg';
+	import hamburgerIcon from '../../static/icons/hamburger.svg';
+</script>
+
+<div class="flex px-8 py-8 justify-between items-center">
+	<div>
+		<img src={logo} alt="logo" />
+	</div>
+	<ul class="sm:flex gap-x-10 hidden">
+		<li>HOME</li>
+		<li>PORTFOLIO</li>
+		<li>CONTACT ME</li>
+	</ul>
+	<div class="sm:hidden">
+		<img src={hamburgerIcon} alt="hamburger icon" />
+	</div>
+</div>
 <h1>Nav Component</h1>
