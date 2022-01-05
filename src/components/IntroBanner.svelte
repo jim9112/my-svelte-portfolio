653a9088a647a14d459945f5254de186fa29b3b1
Jimmy Hannan <jim9112@gmail.com>
2022-01-05 18:02:35 -0500

    responsive images and fonts

--- src/components/IntroBanner.svelte
+++ src/components/IntroBanner.svelte
@@ -1,3 +1,13 @@
 <div>
-	<img src="static/homepage/mobile/image-homepage-hero.jpg" alt="hero" />
+	<img
+		srcset="static/homepage/mobile/image-homepage-hero.jpg 311w, 
+        static/homepage/tablet/image-homepage-hero.jpg 688w,
+        static/homepage/desktop/image-homepage-hero.jpg 1110w"
+		sizes="90vw"
+		alt="hero"
+	/>
+	<h2 class="font-heading font-bold text-4xl">
+		Hey, I’m James Hannan and I love building beautiful websites
+	</h2>
+	<button type="button">About Me</button>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
+		fontFamily: {
+			heading: ['Ibarra Real Nova', 'serif'],
+			body: ['Public Sans', 'sans-serif']
+		},
 		extend: {}
 	},
 
