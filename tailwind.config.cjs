e97d420585b76e79b246b59d141249feb9e04f7d
Jimmy Hannan <jim9112@gmail.com>
2022-01-05 18:10:45 -0500

    add theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,16 @@ const config = {
 			heading: ['Ibarra Real Nova', 'serif'],
 			body: ['Public Sans', 'sans-serif']
 		},
-		extend: {}
+		extend: {
+			colors: {
+				'primary-cyan': '#5FB4A2',
+				'primary-dark-blue': '#203A4C',
+				'primary-greyish-blue': '#33323D',
+				'secondary-very-light-grey': '#FAFAFA',
+				'secondary-light-grey': '#EAEAEB',
+				'secondary-bright-red': '#F43030'
+			}
+		}
 	},
 
 	plugins: []
