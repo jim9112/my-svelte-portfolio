1066fe2eeaa01453998a3ec088016eb81d5b87a2
Jimmy Hannan <jim9112@gmail.com>
2022-01-05 18:27:43 -0500

    add about me component

---
+++ src/components/AboutMe.svelte
@@ -0,0 +1,5 @@
+<script>
+	import me from '../../static/me.jpg';
+</script>
+
+<img src={me} alt="James Hannan" />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script>
+	import AboutMe from '../components/AboutMe.svelte';
 	import IntroBanner from '../components/IntroBanner.svelte';
 </script>
 
 <div class="px-8">
 	<IntroBanner />
+	<AboutMe />
 </div>
