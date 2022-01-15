08a7587d9704e079bc323c01ad35c9008fdddab2
James Hannan <jim9112@gmail.com>
2022-01-14 19:24:06 -0500

    homepage tablet done

--- src/components/ContactMe.svelte
+++ src/components/ContactMe.svelte
@@ -8,7 +8,12 @@
 	};
 </script>
 
-<section class="mt-8 pt-28 pb-20 px-4 text-center flex flex-col place-items-center gap-10">
-	<Heading2>Interested in doing a project together?</Heading2>
+<section
+	class="mt-8 pt-28 pb-20 px-4 text-center flex flex-col place-items-center gap-10 sm:flex-row"
+>
+	<div class="sm:w-[350px]">
+		<Heading2>Interested in doing a project together?</Heading2>
+	</div>
+	<div class="hidden sm:block w-[113px] h-[1px] bg-secondary-light-grey" />
 	<Button clickHandler={onClick}>CONTACT ME</Button>
 </section>
--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,14 +5,14 @@
 </script>
 
 <footer
-	class="bg-primary-greyish-blue text-secondary-very-light-grey flex flex-col place-items-center py-14"
+	class="bg-primary-greyish-blue text-secondary-very-light-grey flex flex-col place-items-center sm:justify-between py-14 sm:py-7 sm:flex-row sm:px-10"
 >
-	<ul class="font-body text-xs text-center flex flex-col gap-8">
+	<ul class="font-body text-xs text-center flex flex-col gap-8 sm:flex-row">
 		<li><a href="/">HOME</a></li>
 		<li><a href="/portfolio">PORTFOLIO</a></li>
 		<li><a href="/contact-me">CONTACT ME</a></li>
 	</ul>
-	<div class="flex gap-4 mt-14">
+	<div class="flex gap-4 mt-14 sm:mt-0">
 		<a href="https://github.com/jim9112" target="blank"
 			><img src={githubIcon} alt="github icon" /></a
 		>
