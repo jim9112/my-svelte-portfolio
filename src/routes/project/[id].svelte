b22bd27495a364e59c89abd9ebc0ecd65092f441
James Hannan <jim9112@gmail.com>
2022-01-15 17:30:16 -0500

    add dynamic page

---
+++ src/routes/project/[id].svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	export const load = ({ params }) => {
+		const id = params.id;
+		return {
+			props: {
+				id
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let id: string;
+</script>
+
+<div>
+	<h1>Individual project page</h1>
+	{id}
+</div>
