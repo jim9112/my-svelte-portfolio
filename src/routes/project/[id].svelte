411208020b7292da26c5125d8d4d5c6fad711173
James Hannan <jim9112@gmail.com>
2022-01-17 15:16:26 -0500

    add first section to project page

--- src/routes/project/[id].svelte
+++ src/routes/project/[id].svelte
@@ -12,16 +12,20 @@
 
 <script lang="ts">
 	import { projects } from '../../stores/projects';
+	import Heading2 from '../../components/global/Heading2.svelte';
+	import Body2 from '../../components/global/Body2.svelte';
 	export let id: string;
 
 	// delete after page is built
 	console.log($projects[id]);
-
 	// get project to display from global state
 	const project = $projects[id];
 </script>
 
-<div>
-	<h1>Individual project page</h1>
-	{project.title}
-</div>
+<section class="px-8">
+	<img src={project.mainImage} alt="" />
+	<div class="border-y-2 border-secondary-light-grey py-6">
+		<Heading2>{project.title}</Heading2>
+		<Body2>{project.description}</Body2>
+	</div>
+</section>
