2d8fd6de0e2e9faf78abf8fa046200b2d1be839f
James Hannan <jim9112@gmail.com>
2022-02-21 19:19:01 -0500

    add repo button to project

---
+++ src/components/project-page/LinkText.svelte
@@ -0,0 +1,3 @@
+<span class="w-fit py-4 px-8 border-[1px] border-primary-greyish-blue text-xs font-body"
+	><slot /></span
+>
--- src/routes/project/[id].svelte
+++ src/routes/project/[id].svelte
@@ -17,12 +17,12 @@
 <script lang="ts">
 	import Heading2 from '../../components/global/Heading2.svelte';
 	import Body2 from '../../components/global/Body2.svelte';
-	import Button from '../../components/global/Button.svelte';
-	import { goto } from '$app/navigation';
+	import LinkText from '../../components/project-page/LinkText.svelte';
 	export let id: string;
 	export let projects;
 
 	let project = projects[id];
+	console.log(project);
 
 	// delete after page is built
 	// get project to display from global state
@@ -33,15 +33,28 @@
 	};
 </script>
 
-<section class="px-8">
+<section class="px-40">
 	{#if project}
-		<img src={project.data.featured_image.url} alt="" />
-		<div class="border-y-2 border-secondary-light-grey py-6">
-			<Heading2>{project.data.project_name[0].text}</Heading2>
-			<Body2>{project.data.project_description[0].text}</Body2>
-			<a href={project.data.live_site_link.url} target="blank"
-				><Button {clickHandler}>Visit Website</Button></a
-			>
+		<div class="h-[500px] w-full relative">
+			<img
+				class="w-full h-full object-top object-cover"
+				src={project.data.featured_image.url}
+				alt=""
+			/>
+		</div>
+		<div class="py-28">
+			<div class="border-y-2 border-secondary-light-grey py-6 w-[350px]">
+				<Heading2>{project.data.project_name[0].text}</Heading2>
+				<Body2>{project.data.project_description[0].text}</Body2>
+				<div class="flex gap-2 pt-5">
+					<a href={project.data.live_site_link.url} target="blank"
+						><LinkText>VISIT WEBSITE</LinkText></a
+					>
+					<a href={project.data.github_repo_link.url} target="blank"
+						><LinkText>VISIT REPO</LinkText></a
+					>
+				</div>
+			</div>
 		</div>
 	{/if}
 </section>
