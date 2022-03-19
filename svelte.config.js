617a4852f57a48755fc6e8962446bd8dcc49c376
James Hannan <jim9112@gmail.com>
2022-03-19 09:11:46 -0400

    remove chanes

--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,7 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		prerender: {
-			crawl: true,
-			enabled: true,
-			onError: 'continue',
-			entries: ['*']
-		}
+		target: '#svelte'
 	}
 };
 
